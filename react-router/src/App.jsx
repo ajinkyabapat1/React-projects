04bae680b68af505219ae03759546bb34b4b9119
ajinkyabapat1 <ajinkyabapat1@gmail.com>
2023-12-12 16:13:07 +0530

    added router structure and fixed console bugs

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-30xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+     
+      
+    </>
   )
 }
 
