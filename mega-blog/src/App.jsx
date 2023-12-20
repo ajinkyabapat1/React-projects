8ae1b635e14feab439429a1485134312649be904
ajinkyabapat1 <ajinkyabapat1@gmail.com>
2023-12-20 19:20:59 +0530

    added basic ui achitecture without css

--- mega-blog/src/App.jsx
+++ mega-blog/src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appWrite/Auth/auth";
+import { login, logout } from "./store/authSlice";
+import {Header,Footer} from './components/index'
 function App() {
-  const [count, setCount] = useState(0)
-console.log(import.meta.env.VITE_APPWRITE_URL)   // why import meta becuse project created using vite else it could be process.env
-  return (
-   <>
-   
-   </>
-  )
+  //console.log(import.meta.env.VITE_APPWRITE_URL)   // why import meta becuse project created using vite else it could be process.env
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  },[]);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        {/* <main>
+        TODO:  <Outlet />
+        </main> */}
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
-export default App
+export default App;
---
+++ mega-blog/src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+export {Header,Footer,Logo,Container,LogoutBtn,Button,Input}
