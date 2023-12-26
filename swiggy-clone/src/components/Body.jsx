e82fdad5137f263b5905ebf136723138d28af851
ajinkyabapat1 <ajinkyabapat1@gmail.com>
2023-12-27 01:40:27 +0530

    added shimmer while loading pages

--- swiggy-clone/src/components/Body.jsx
+++ swiggy-clone/src/components/Body.jsx
@@ -1,6 +1,6 @@
 import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
-import { resList } from "../utils/mockData";
+import Shimmer from "./Shimmer";
 const Body = () => {
   const [listOfRestaurants, setRestList] = useState([]);
   const filterRestaurant = () => {
@@ -17,8 +17,13 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
+   
     setRestList(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
   };
+  if(listOfRestaurants.length==0){
+   return  <Shimmer/>
+   
+  }
   return (
     <div className="body">
       <div className="filter">
