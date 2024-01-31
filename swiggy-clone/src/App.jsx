26ca7e594a9e2707d96b5da7439fd141ec8cabd7
ajinkyabapat1 <ajinkyabapat1@gmail.com>
2024-01-31 23:48:18 +0530

    added lazy loading

--- swiggy-clone/src/App.jsx
+++ swiggy-clone/src/App.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
-import About from "./components/About";
+//import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
-import RestaurantMenu from "./components/RestaurantMenu";
+//import RestaurantMenu from "./components/RestaurantMenu";
 import Shimmer from "./components/Shimmer";
 // import Grocery from "./components/Grocery";
-const Grocery=lazy(()=>import('./components/Grocery'));
+const Grocery = lazy(() => import("./components/Grocery"));
+const RestaurantMenu=lazy(()=>import('./components/RestaurantMenu'))
+const About=lazy(()=>import('./components/About'))
+
 const AppLayout = () => {
   return (
     <div className="app">
@@ -25,10 +28,17 @@ const appRouter = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { path: "/", element: <Body /> },
-      { path: "about", element: <About /> },
+      { path: "about", element: <Suspense fallback={<Shimmer />}><About/></Suspense> },
       { path: "contact", element: <Contact /> },
-       {path:"restaurant/:resId",element:<RestaurantMenu/>},
-       {path:"grocery",element:<Suspense fallback={<Shimmer/>}><Grocery/></Suspense>}
+      { path: "restaurant/:resId", element: <Suspense fallback={<Shimmer />}><RestaurantMenu/></Suspense> },
+      {
+        path: "grocery",
+        element: (
+          <Suspense fallback={<Shimmer />}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
     ],
     errorElement: <Error />,
   },
